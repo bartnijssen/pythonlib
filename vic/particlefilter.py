3003cc62a6ec73506d2f0d16431c982677807813
Bart Nijssen <nijssen@uw.edu>
2013-09-01 13:08:51 -0600

    Added ConfigParser to particlefilter.py

--- vic/particlefilter.py
+++ vic/particlefilter.py
@@ -1,13 +1,18 @@
 # This code is to support a particle filter implementation using VIC
-#import datetime as dt
+from __future__ import division
+import collections
+import ConfigParser
+import datetime as dt
 #import matplotlib as mpl
 #import matplotlib.pyplot as plt
 import multiprocessing
-#import numpy as np
+import numpy as np
 #import pandas as pd
+import re
 import subprocess
 #import sys
 from utils.general import flatten
+from utils.parse import parse_date
 #import uuid
 
 class Error(Exception):
@@ -18,12 +23,13 @@ class SimulationError(Error):
     """Exception raised for simulation errors"""
     pass
 
-class Simulation:
+class Simulation(object):
     """Model simulation"""
-    def __init__(self, exe=None, args=None, logfile=None):
+    def __init__(self, exe=None, configfile=None, args=None, logfile=None):
         """Initiate a simulation instance"""
         self.exe = exe
         self.args = args
+        self.configfile = configfile
         self.logfile = logfile
 
     def __str__(self):
@@ -31,6 +37,7 @@ class Simulation:
         bstr += 'arguments:\n'
         if self.args:
             bstr += ', '.join(self.args)
+        bstr += '\nconfigfile: {}'.format(self.configfile)
         bstr += '\nlogfile: {}'.format(self.logfile)
         return bstr
 
@@ -64,46 +71,153 @@ class Simulation:
         if type(self.logfile) is str:
             loghandle.close()
 
-class Particle():
+class VicSimulation(Simulation):
+    """VIC simulation class"""
+    def writeconfig(self, config):
+        repl = re.compile('^OUTVAR\d*$')
+        with open(self.configfile, 'w') as f:
+            for key, value in config.items():
+                f.write('{:20s} {}\n'.format(re.sub(repl, 'OUTVAR', key),
+                        value))
+
+
+class RouteSimulation(Simulation):
+    """route simulation class"""
+    pass
+
+class ParticleFilter(object):
+    """Particle filter"""
+    def __init__(self, configurationfile):
+        """Initialize a particle filter based on a configurationfile"""
+        self.pool = None
+        # Parse the configuration file
+        configparser = ConfigParser.SafeConfigParser(allow_no_value=True)
+        configparser.optionxform = str # preserve case of configuration keys
+        print 'Reading {}'.format(configurationfile)
+        configparser.read(configurationfile)
+        self.config = collections.OrderedDict()
+        for section in configparser.sections():
+            self.config[section] = collections.OrderedDict()
+            for key, value in configparser.items(section):
+                self.config[section][key] = value
+        if not self.config['COMPUTING']['poolsize']:
+            self.poolsize = multiprocessing.cpu_count()
+        else:
+            self.poolsize = configparser.getint('COMPUTING','poolsize')
+
+        # initialize simulation time
+        self.start = parse_date(self.config['SIMULATION']['start'])
+        self.now = self.start
+        self.end = parse_date(self.config['SIMULATION']['end'])
+        self.timeinterval = dt.timedelta(configparser.getint('SIMULATION',
+                                                             'interval'))
+
+        # Initialize particles
+        self.np = configparser.getint('FILTER', 'np')
+        self.particles = [VicParticle(i+1, 1./self.np) for i in range(self.np)]
+        for i in range(self.np):
+            p = self.particles[i]
+            p.vic['exe'] = self.config['PATHS']['vicexe']
+            p.vic['infile'] = self.config['PATHS']['vicinfiletemplate'].format(i+1)
+            p.vic['logfile'] = self.config['PATHS']['viclogfiletemplate'].format(i+1)
+            p.route['exe'] = self.config['PATHS']['routeexe']
+            p.route['infile'] = self.config['PATHS']['routeinfiletemplate'].format(i+1)
+            p.route['logfile'] = self.config['PATHS']['routelogfiletemplate'].format(i+1)
+        self.normalize_weights()
+
+    def __repr__(self):
+        bstr = '{}\n'.format(self.config)
+        bstr += '{}: {}\n'.format(self.poolsize, self.pool)
+        bstr += '{}: {}\n'.format(self.np, [self.particles[i] for i in range(self.np)])
+        bstr += '{} -- {}({}) -- {}\n'.format(self.start, self.now,
+                                              self.timeinterval, self.end)
+        return bstr
+
+    def advance(self):
+        # Advance all the particles
+        self.config['VICCONFIG']['STARTYEAR'] = self.now.year
+        self.config['VICCONFIG']['STARTMONTH'] = self.now.month
+        self.config['VICCONFIG']['STARTDAY'] = self.now.day
+        endsimulation = self.now + self.timeinterval
+        self.config['VICCONFIG']['ENDYEAR'] = endsimulation.year
+        self.config['VICCONFIG']['ENDMONTH'] = endsimulation.month
+        self.config['VICCONFIG']['ENDDAY'] = endsimulation.day
+
+        self.pool = multiprocessing.Pool(self.poolsize)
+        results = [self.pool.apply_async(advance, [self.particles[i], self.config['VICCONFIG']])
+                   for i in range(self.np)]
+        self.pool.close()
+        self.pool.join()
+        # Report errors
+        for r in results:
+            if r.get() is not None:
+                raise(SimulationError(r.get()))
+        # Update the simulation time
+        self.now += self.timeinterval
+
+    def evaluate(self):
+        for p in self.particles:
+            p.evaluate()
+
+    def normalize_weights(self):
+        total = np.array([self.particles[i].weight for i in range(self.np)]).sum()
+        for p in self.particles:
+            p.weight /= total
+
+    def resample(self):
+        pass
+
+class Particle(object):
     """Single particle in a particular filter"""
 
-    def __init__(self, ID=None, weight=None, state=None):
-        """Initiate a particle"""
-#        self.ID = uuid.uuid1()
+    def __init__(self, ID, weight, state=None):
+        """Initialize a particle"""
         self.ID = ID
         self.weight = weight
         self.state = state
-
-#    def __str__(self):
-#        bstr = '{}({}): {}'.format(str(self.ID)[:8], self.weight, self.state)
-#        return bstr
+        print "initialized particle {}".format(self.ID)
 
     def __repr__(self):
         bstr = '{}({}): {}'.format(self.ID, self.weight, self.state)
         return bstr
 
-    def advance(self, exe, infile, logfile):
+    def advance(self):
         """Advance a particle"""
-        self.simulation = Simulation(exe, ['-g', infile], logfile)
-        self.simulation.run()
+        pass
 
     def evaluate(self):
         """Evaluate a particle (calculate its weight)"""
         pass
 
-def advance(particle, exe, infile, logfile):
-    particle.advance(exe, infile, logfile)
+class VicParticle(Particle):
+    """Particle for running VIC simulations"""
+
+    def __init__(self, ID, weight, state=None):
+        # invoke the parent's initializer
+        super(VicParticle, self).__init__(ID, weight, state)
+        self.vic = {'exe': None, 'infile': None, 'logfile': None}
+        self.route = {'exe': None, 'infile': None, 'logfile': None}
+
+    def advance(self, config):
+        """Advance a particle"""
+        self.vicsim = VicSimulation(self.vic['exe'], self.vic['infile'],
+                                    ['-g', self.vic['infile']],
+                                    self.vic['logfile'])
+        self.vicsim.writeconfig(config)
+        # self.vicsim.run()
+        # self.routesim = RouteSimulation(rout['exe'], [route['infile'], route['logfile']).run()
+        # self.routesim.run()
+
+
+def advance(particle, config):
+    particle.advance(config)
 
 if __name__ == '__main__':
-
-    pool = multiprocessing.Pool()
-
-    exe = '/d1/nijssen/vicpf/bin/vicNl'
-    infiletemplate = '/d1/nijssen/vicpf/config/gunnison/vic/gunnison.vic.global.test_{:03d}'
-    logfiletemplate = '/d1/nijssen/junk/run_{:03d}'
-
-    Np = 10
-    particles = [Particle(i+1, 1./Np) for i in range(Np)]
-    results = [pool.apply_async(advance, [particles[i], exe, infiletemplate.format(i+1), logfiletemplate.format(i+1)]) for i in range(Np)]
-    pool.close()
-    pool.join()
+    pf = ParticleFilter('/d1/nijssen/vicpf/config/gunnison/particlefilter/gunnison_pf.cfg')
+    print pf
+    print "Advancing filter 1"
+    pf.advance()
+#    print pf
+#    print "Advancing filter 2"
+#    pf.advance()
+#    print pf
